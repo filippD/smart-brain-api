f542c7f5287a6857fc881937ad70c89342cb286a
YellowPipe <philippdmitrienko@gmail.com>
2019-08-23 16:57:35 +0300

    updating database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-shallow-46533',
-    user : 'philipp',
-    password : '',
-    database : 'db'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
